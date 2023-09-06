25538dc864011d944434e7e6e9703a444b27eda0
Diego <diego_san26@hotmail.com>
2023-09-05 22:42:49 -0500

    Mouse follower finished

---
+++ projects/mouse-follower/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+import { FollowMouse } from './FollowMouse'
+
+function App () {
+  return (
+    <FollowMouse />
+  )
+}
+
+export default App
---
+++ projects/mouse-follower/src/FollowMouse.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+
+export const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('efecto ', { enabled })
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+      setPosition({ x: 0, y: 0 })
+    }
+  }, [enabled])
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        borderRadius: '50%',
+        border: '1px solid #fff',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => { setEnabled(!enabled) }}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
+  )
+}
